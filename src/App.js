de322cf39d3ef0096b0e5e124397987f7550a988
Maria Popa <maria_olimp@yahoo.com>
2022-08-05 19:02:37 +0300

    fuctional routes

--- src/App.js
+++ src/App.js
@@ -1,54 +1,43 @@
 import './App.css';
-import Game from './components/Game';
 import Button from 'react-bootstrap/Button';
-import { useRef } from 'react';
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
 
 function App() {
 
-  const player1Input = useRef(null);
-  const player2Input = useRef(null);
+  // const player1Input = useRef(null);
+  // const player2Input = useRef(null);
+
+  const [player1Input, setPlayer1Input] = useState('');
+  const [player2Input, setPlayer2Input] = useState('');
 
   const navigate = useNavigate();
 
 
   const handleClick = () => {
-    const player1 = player1Input.current.value;
-    const player2 = player2Input.current.value;
-    console.log(player1)
-    console.log(player2)
-    navigate('/game');
-    // retrun (<Game first = { player1 } second = { player2 } />);
+    console.log(player1Input);
+    console.log(player2Input);
+    navigate('/game', {state: {player1: player1Input, player2: player2Input}});
   }
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={
-          <div className='col position-absolute top-50 start-50 translate-middle text-center d-grid gap-3'>
-            <h1>Tic Tac Toe</h1>
-            <div>
-              <label className='m-2'>Plese enter player 1 name: </label>
-              <input type='text' ref={player1Input} />
-            </div>
-            <div>
-              <label className='m-2'>Plese enter player 2 name: </label>
-              <input type='text' ref={player2Input} />
-            </div>
-            <div className='text-center d-flex justify-content-center'>
-              <Button variant="light" size='lg' onClick={handleClick} >
-                Start the game
-              </Button>
-            </div>
-          </div>
-        } />
-        <Route path='/game' element={<Game first={player1Input.current.value} second={player2Input.current.value}/>} />
-
-        {/* </Route> */}
-      </Routes>
-    </BrowserRouter>
+    <div className='col position-absolute top-50 start-50 translate-middle text-center d-grid gap-3'>
+      <h1>Tic Tac Toe</h1>
+      <div>
+        <label className='m-2'>Plese enter player 1 name: </label>
+        <input type='text' placeholder='Your nickname...' onChange={e => setPlayer1Input(e.target.value)} />
+      </div>
+      <div>
+        <label className='m-2'>Plese enter player 2 name: </label>
+        <input type='text' placeholder='Your nickname...' onChange={e => setPlayer2Input(e.target.value)} />
+      </div>
+      <div className='text-center d-flex justify-content-center'>
+        <Button variant="light" size='lg' onClick={handleClick} >
+          Start the game
+        </Button>
+      </div>
+    </div>
   );
-  // <Game />
 }
 
 export default App;
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Table from "./Table";
 
 const Game = () => {
+    const location = useLocation();
+    const {player1, player2} = location.state;
+
+    console.log('p1(from Game): ', player1);
+    console.log('p2(from Game): ', player2);
+
+    const turn1 = true;
+    const gameTurn = (turn=true) => {
+        return turn ? player1 : player2;
+    }
 
 
     return (
         <div className='position-absolute top-50 start-50 translate-middle'>
             <h1 className='d-flex justify-content-center fs-1'>Tic Tac Toe</h1>
             <Table />
-            <span className='fs-5'>Player: 1</span>
+            <label className='fs-5'>Player: {gameTurn(turn1)}</label>
         </div>
     )
 }
